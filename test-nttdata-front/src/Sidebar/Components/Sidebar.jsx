c25372cbdc0bfdeb664fe13010d3f58e7c96c216
Andre Cruz <ajcruzg95@gmail.com>
2023-11-08 09:21:27 -0500

    colores

--- test-nttdata-front/src/Sidebar/Components/Sidebar.jsx
+++ test-nttdata-front/src/Sidebar/Components/Sidebar.jsx
@@ -6,14 +6,14 @@ export const Sidebar = (props) => {
     <>
         <div style={{height: '100vh', overflow:'hidden'}}>
             <div className='min-h-screen flex relative lg:static surface-ground' >
-                <div className='h-screen hidden lg:block flex-shrink-0 absolute lg:static left-0 top-0 z-1 select-none' style={{width:'280px',backgroundColor:"#805AD5"}}>
+                <div className='h-screen hidden lg:block flex-shrink-0 absolute lg:static left-0 top-0 z-1 select-none' style={{width:'280px',backgroundColor:"#2E4990"}}>
                     <div className='flex flex-column h-full'>
                         <div className='flex align-items-center px-5 flex-shrink-0 border-bottom-1' style={{height: "60px",backgroundColor:"#805AD5"}}>
                             <span className='text-white font-bold'>TEST NTTDATA</span>
                         </div>
                         <div className='overflow-y-auto mt-3'>
                             <ul className='list-none p-3 m-0'>
-                                <a className='p-ripple flex align-items-center cursor-pointer p-3 hover:bg-purple-800 border-round text-bluegray-100 hover:text-bluegray-50 transition-duration-150 transition-colors w-full'>
+                                <a className='p-ripple flex align-items-center cursor-pointer p-3 hover:bg-blue-900 border-round text-bluegray-100 hover:text-bluegray-50 transition-duration-150 transition-colors w-full'>
                                     <i className='pi pi-users'/>
                                     <span className='font-medium ml-2'>Empleados</span>
                                 </a>
